78b8116241fd1be2515c1b3a495ff733e1c487b6
朱念洋 <dantezhu@dantezhuMBP.lan>
2017-02-15 15:54:02 +0800

    cli

--- realtime_game_prototype/manage.py
+++ realtime_game_prototype/manage.py
@@ -1,1 +1,28 @@
 # -*- coding: utf-8 -*-
+
+import click
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+def runserver():
+    """
+    启动服务器
+    """
+    pass
+
+
+@cli.command()
+def runclient():
+    """
+    启动客户端
+    """
+    pass
+
+
+if __name__ == '__main__':
+    cli()
