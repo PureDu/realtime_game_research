bb341bab53184e289d9694a2da23472a4f8ffdfe
朱念洋 <dantezhu@dantezhuMBP.lan>
2017-02-16 16:34:10 +0800

    log

--- realtime_game_prototype/share/constants.py
+++ realtime_game_prototype/share/constants.py
@@ -7,6 +7,11 @@ KERNEL_FRAME_RATE = 20
 SHOW_FRAME_RATE = 60
 
 # 日志
-LOG_FORMAT = '[%(levelname)s][%(funcName)s]:%(message)s'
+LOG_FORMAT = '\n'.join((
+    '/' + '-' * 80,
+    '[%(levelname)s][%(asctime)s][%(process)d:%(thread)d][%(filename)s:%(lineno)d %(funcName)s]:',
+    '%(message)s',
+    '-' * 80 + '/',
+))
 COLOR_LOG_FORMAT = '%(log_color)s' + LOG_FORMAT
 
